7a31c30d0c4546553232dee947957622aa039081
Alexey Dyakonov <dyakonov261@gmail.com>
2024-09-19 21:30:42 +0500

    Загрузка дошнего задания

--- config/settings.py
+++ config/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hw_django',
+        'USER': 'postgres',
+        'PASSWORD': '4451',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432'
     }
 }
 
@@ -83,6 +87,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
