06863fff724ac9dafebce0dcbc3fc5302b3962a1
Alexey Dyakonov <dyakonov261@gmail.com>
2024-10-21 23:39:03 +0500

    Forms

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
-
+from catalog.models import Product, Category, Version
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -15,4 +14,12 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "name_category",
+    )
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ("product",
+                    "version_number",
+                    "version_name",
+                    "sign"
     )
